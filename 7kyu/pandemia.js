3543a3b0d4e58b0a699349d8cd7779b9626dcd92
aubreywullschleger <aubreywullschleger@users.noreply.github.com>
2020-02-13 21:00:29 -0600

    pandemia refactor

--- 7kyu/pandemia.js
+++ 7kyu/pandemia.js
@@ -1,25 +1,14 @@
+const populationCounter = (accumulator, countries) => accumulator + countries.length
+
 function infected(s) {
   let continents = s.split("X")
-  let totalPopulation = 0
-  let infectedCountries = 0
-  let infectedPopulation = 0
-
-  for (countries in continents) {
-    totalPopulation += continents[countries].length
-    if (continents[countries].includes("1")) {
-      let infectedCountry = continents[countries].replace(/0/gi, "1")
-      infectedCountries += infectedCountry.length
-    }
-  }
+  let totalPopulation = continents.reduce(populationCounter, 0)
+  let infectedCountries = continents.filter(countries => parseInt(countries, 2) > 0)
+  let infectedPopulation = infectedCountries.reduce(populationCounter, 0)
   
-  infectedPopulation = (infectedCountries / totalPopulation) * 100;
-
-  if (infectedCountries === 0) {
-    infectedPopulation = 0
-  }
-  
-  return infectedPopulation
+  return infectedPopulation === 0 ? 0 : (infectedPopulation / totalPopulation) * 100
 }
+
 
 console.log(infected("01000000X000X011X0X")) // 73.33333333333333
 console.log(infected("XXXXX")) // 0
