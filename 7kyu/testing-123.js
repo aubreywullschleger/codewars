36e3013b9baef1aac8ead13ffb48301efd63d279
aubreywullschleger <aubreywullschleger@users.noreply.github.com>
2020-02-20 22:46:16 -0600

    testing-1-2-3 refactor, map will return empty array if mapping over empty array

--- 7kyu/testing-123.js
+++ 7kyu/testing-123.js
@@ -1,4 +1,4 @@
-const number = array => !array.length ? [] : array.map((el, idx) => `${idx + 1}: ${el}`)
+const number = array => array.map((el, idx) => `${idx + 1}: ${el}`)
 
 console.log(number(["a", "b", "c"]))
 console.log(number([]))
