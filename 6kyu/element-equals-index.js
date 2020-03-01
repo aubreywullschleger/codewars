24a30a0ee5fad9ad6c10e6ab9db9b3e96705ee89
aubreywullschleger <aubreywullschleger@users.noreply.github.com>
2020-02-29 22:57:55 -0600

    alt solution using binary search + linear search

--- 6kyu/element-equals-index.js
+++ 6kyu/element-equals-index.js
@@ -6,3 +6,35 @@ const indexEqualsValue = a => {
   }
   return -1;
 }
+
+// const indexEqualsValue = a =>{
+//   let low = 0
+//   let high = a.length - 1
+//   let mid = Math.round((low + high)/ 2)
+
+//   while (mid !== a[mid]) {
+//     if (high >= low) {
+//       if (mid > a[mid]) { 
+//         low = mid + 1
+//         high = high
+//         mid = Math.round((low + high)/ 2)  
+//       } else {
+//         low = low
+//         high = mid - 1
+//         mid = Math.round((low + high)/ 2)
+//       }
+//     } else if (high === mid || low === mid) {
+//       return -1
+//     }
+//   }
+//   if (mid === a[mid]) {
+//     for (let i = 0; i < mid; i++) {
+//       if(a[i] === i) {
+//         return i
+//       }
+//     }
+//     return a[mid]
+//   }
+// }
+
+indexEqualsValue([-5, 1, 2, 3, 4, 5, 7, 10, 15]) // 1
