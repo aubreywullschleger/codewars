d13d8e4a9c531f90adcbe8d0511e9f9acbd41b7d
aubreywullschleger <aubreywullschleger@users.noreply.github.com>
2020-03-04 15:14:52 -0600

    shortest word refactor

--- 7kyu/shortest-word.js
+++ 7kyu/shortest-word.js
@@ -1,12 +1,16 @@
-function findShort(s){
-  let wordArray = s.split(" ")
-  let shortWord = wordArray[0].length
-  for (let i = 0; i < wordArray.length; i++) {
-    if (shortWord > wordArray[i].length) {
-      shortWord = wordArray[i].length
-    }
-  }
-  return shortWord
-}
+// function findShort(s){
+//   let wordArray = s.split(" ")
+//   let shortWord = wordArray[0].length
+//   for (let i = 0; i < wordArray.length; i++) {
+//     if (shortWord > wordArray[i].length) {
+//       shortWord = wordArray[i].length
+//     }
+//   }
+//   return shortWord
+// }
+
+// refactor
+// Math.min returns the min value number passed to it with comma separated values
+const findShort = s => Math.min(...s.split(" ").map(word => word.length))
 
 findShort("turns ou random test cases are easier than writing out basic ones")
